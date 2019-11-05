1bd48ccf1e4dddc585752b33bfaae47622f8fd5e
Ann Cerveny <cervann11@gmail.com>
2019-11-04 22:20:22 -0700

    Add ResultsContainer and Result components

---
+++ src/components/Result.vue
@@ -0,0 +1,14 @@
+<template>
+  <article id="Result">
+  </article>
+</template>
+
+<script>
+export default {
+  
+}
+</script>
+
+<style scoped>
+
+</style>
--- src/components/SearchForm.vue
+++ src/components/SearchForm.vue
@@ -1,9 +1,16 @@
 <template>
-  <form id="SearchForm" @submit.prevent="searchTerm">
-    <h1>Search Term</h1>
-    <input type="text" placeholder="Enter search term..." v-model="term" />
-    <button type="submit" v-on:click="searchTerm">Submit</button>
-  </form>
+  <main>
+    <section id="SearchContainer">
+      <form id="SearchForm" @submit.prevent="searchTerm">
+        <h1 id="search-prompt">Search Term</h1>
+        <input id="search-input" type="text" placeholder="Enter search term..." v-model="term" />
+        <button type="submit" @click="searchTerm">Submit</button>
+      </form>
+    </section>
+    <section>
+    <ResultsContainer />
+    </section>
+  </main>
 </template>
 
 <script>
@@ -11,7 +18,8 @@ export default {
   name: 'SearchForm',
   data() {
     return {
-      term: ""
+      term: "",
+      results: []
     }
   },
   methods: {
@@ -19,11 +27,51 @@ export default {
       const url = `https://dictionaryapi.com/api/v3/references/thesaurus/json/${this.term}?key=70844809-6b63-4d12-b44a-8f4ca91522b5`
       fetch(url)
       .then(response => response.json())
-      .then(data => console.log(data))
+      .then(data => this.results = data[0].meta.syns[0])
+      .catch(error => console.log(error))
     }
   }
 }
 </script>
 
 <style scoped>
+
+#SearchContainer {
+  height: 30vh;
+  width: 100%;
+  background-color: #E1A953;
+  display: flex;
+}
+
+#SearchForm {
+  width: 50%;
+  height: 200px;
+  border: 2px solid black;
+  border-radius: 10px;
+  margin: auto;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-evenly;
+  align-items: center;
+  font-family: 'McLaren', cursive;
+  background-color: #014b0d;
+}
+
+#search-prompt {
+  font-size: 30px;
+  color: #E1A953;
+  text-shadow:
+		-1px -1px 0 #000,
+		1px -1px 0 #000,
+		-1px 1px 0 #000,
+		1px 1px 0 #000;
+}
+
+#search-input {
+  height: 40px;
+  width: 50%;
+  border-radius: 10px;
+  font-size: 20px;
+  padding: 10px;
+}
 </style>
