153354324a32c2c45cedfed32c51628f287d2d59
Ann Cerveny <cervann11@gmail.com>
2019-11-04 19:02:09 -0700

    Add SearchForm component that includes input and button

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,24 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <SearchForm />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import SearchForm from './components/SearchForm'
 
 export default {
   name: 'app',
   components: {
-    HelloWorld
+    SearchForm
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+ * {
+   box-sizing: border-box;
+   margin: 0;
+   padding: 0;
+ }
 </style>
---
+++ src/components/SearchForm.vue
@@ -0,0 +1,31 @@
+<template>
+  <form id="SearchForm">
+    <h1>Search Term</h1>
+    <input type="text" placeholder="Enter search term..." v-model="term" />
+    <button type="submit" v-on:click="searchTerm(term)">Submit</button>
+  </form>
+</template>
+
+<script>
+export default {
+  name: 'SearchForm',
+  data() {
+    return {
+      term: ""
+    }
+  },
+  // methods: {
+  //   async searchTerm() {
+  //     const url = "https://dictionaryapi.com/api/v3/references/thesaurus/json/"
+  //     const key = "?key=70844809-6b63-4d12-b44a-8f4ca91522b5"
+  //     fetch(url + `${term}` + key) {
+
+  //     }
+  //   }
+  // }
+  
+}
+</script>
+
+<style scoped>
+</style>
